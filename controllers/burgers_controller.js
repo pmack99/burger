262059aefb72b93fbbfc276e1d5a667d22b11079
PMack <prestonmack@gmail.com>
2018-11-22 10:42:22 -0500

    loads, but no functions

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,12 +1,11 @@
 var express = require("express");
-
 var router = express.Router();
 
 // Import the model to use its database functions.
 var burger = require("../models/burger.js");
 
 router.get("/", function (req, res) {
-    burger.all(function (data) {
+    burger.selectAll(function (data) {
         var hbsObject = {
             burger: data
         };
@@ -15,30 +14,27 @@ router.get("/", function (req, res) {
     });
 });
 
-router.post("/api/cats", function (req, res) {
-    burger.create([
+router.post("/api/burgers/", function (req, res) {
+    burger.createBurger([
         "burger_name"
     ], [
             req.body.name,
         ], function (result) {
-            // Send back the ID of the new quote
+            // Send back the ID of the new burger
             res.json({ id: result.insertId });
         });
 });
 
+router.put('/api/burgers/:id', function(req, res) {
+    var condition = 'id = ' + req.params.id;
+  
+    burger.devourBurger({
+      devoured: true
+    }, condition, function(data) {
+      res.redirect('/');
+    });
+  });
+  
+  // Export routes for server.js to use.
+  module.exports = router;
 
-
-cat.update({
-    devoured: req.body.devoured
-}, condition, function (result) {
-    if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-    } else {
-        res.status(200).end();
-    }
-});
-
-
-// Export routes for server.js to use.
-module.exports = router;
