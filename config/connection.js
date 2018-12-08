10eb4a18e6fbb4f0844a1f41e06e72f95aae68a1
PMack <prestonmack@gmail.com>
2018-12-08 11:56:55 -0500

    f

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if(process.env.NODE_ENV === "production"){
+  var connectionoptions = process.env.JAWSDB_URL
+
+}else{
+  var connectionoptions = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  }
+  
+}
+
+var connection = mysql.createConnection(connectionoptions);
+
+
+
+
+
 
 // Make connection.
 connection.connect(function(err) {
